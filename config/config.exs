3988584281e242ffd158f34f16e6368e99d28e84
Malcolm Roberts <maroberts@gmail.com>
2014-09-07 19:15:27 -0500

    added logic to pull in the weather for a given airport code.

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :weather, noaa_url: "http://w1.weather.gov/xml/current_obs"
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
